f3416b6d8ab082a7c884a38f3f27b64c3479dcd1
VladislavChernyshev <v.chernyshev911@gmail.com>
2022-07-12 22:39:03 +1000

    lesson 3

--- geek-cloud-application/src/main/java/geekbrains/cloud/controllers/MainController.java
+++ geek-cloud-application/src/main/java/geekbrains/cloud/controllers/MainController.java
@@ -1,6 +1,10 @@
 package geekbrains.cloud.controllers;
 
+import geekbrains.cloud.model.AbstractMessage;
+import geekbrains.cloud.model.FileMessage;
+import geekbrains.cloud.model.ListMessage;
 import geekbrains.cloud.network.Net;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
@@ -10,6 +14,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
@@ -17,95 +22,97 @@ import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
 
-    private File clientDir;
+    private Path clientDir;
 
+    public ListView<String> clientView;
+    public ListView<String> serverView;
     private Net net;
 
-    public ListView<String> view;
-
-    public TextField input;
+ 
 
 
-    private void readListFiles() {
-        try {
-//            view.getItems().clear();
-//            Long filesCount = net.readLong();
-//            for (int i = 0; i < filesCount; i++) {
-//                String fileName = net.readUtf();
-//                view.getItems().addAll(fileName);
-//            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
     private void read() {
         try {
             while (true) {
-                String command = net.readUtf();
-                if (command.equals("#list#")) {
-                    readListFiles();
-                }
-                if (command.equals("#status#")){
-                    String status = net.getInputStream().readUTF();
-                    input.setText(status);
-                }
+               AbstractMessage message = net.read();
+               if (message instanceof ListMessage lm){
+                   serverView.getItems().clear();
+                  serverView.getItems().addAll(lm.getFiles());
+               }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    private List<String> getClientFiles() throws IOException {
+        return Files.list(clientDir)
+                .map(Path::getFileName)
+                .map(Path::toString)
+                .toList();
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         try {
-            clientDir = new File("files");
+            clientDir = Path.of("files");
+            clientView.getItems().clear();
+            clientView.getItems().addAll(getClientFiles());
             net = new Net("localhost", 8189);
             Thread readThread = new Thread(this::read);
             readThread.setDaemon(true);
             readThread.start();
 
-            view.getItems().addAll(getClientFiles());
-
-            view.setOnMouseClicked(mouseEvent -> {
-                if (mouseEvent.getClickCount() == 2) {
-                    String fileName = view.getSelectionModel().getSelectedItem();
-                    try {
-                        sendFile(fileName);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
+//            clientView.getItems().addAll(getClientFiles());
+//
+//            clientView.setOnMouseClicked(mouseEvent -> {
+//                if (mouseEvent.getClickCount() == 2) {
+//                    String fileName = clientView.getSelectionModel().getSelectedItem();
+//                    try {
+//                        sendFile(fileName);
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            });
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private List<String> getClientFiles() {
-        String[] files = clientDir.list();
-        if (files == null) {
-            return List.of();
-        }
-        return Arrays.stream(files)
-                .toList();
+//    private List<String> getClientFiles() {
+//        String[] files = clientDir.list();
+//        if (files == null) {
+//            return List.of();
+//        }
+//        return Arrays.stream(files)
+//                .toList();
+//    }
+
+//    private void sendFile(String fileName) throws IOException {
+//        net.getOutputStream().writeUTF("#file#");
+//        net.getOutputStream().writeUTF(fileName);
+//
+//        File file = clientDir.toPath().resolve(fileName).toFile();
+//        net.getOutputStream().writeLong(file.length());
+//        byte[] buffer = new byte[256];
+//        try (InputStream fis = new FileInputStream(file)) {
+//            while (fis.available() > 0){
+//                int readCount = fis.read(buffer);
+//                net.getOutputStream().write(buffer, 0, readCount);
+//            }
+//        }
+//        net.getOutputStream().flush();
+//    }
+
+    public void upload(ActionEvent actionEvent) throws IOException {
+        String fileName = clientView.getSelectionModel().getSelectedItem();
+        net.write(new FileMessage(clientDir.resolve(fileName)));
     }
 
-    private void sendFile(String fileName) throws IOException {
-        net.getOutputStream().writeUTF("#file#");
-        net.getOutputStream().writeUTF(fileName);
+    public void download(ActionEvent actionEvent) {
 
-        File file = clientDir.toPath().resolve(fileName).toFile();
-        net.getOutputStream().writeLong(file.length());
-        byte[] buffer = new byte[256];
-        try (InputStream fis = new FileInputStream(file)) {
-            while (fis.available() > 0){
-                int readCount = fis.read(buffer);
-                net.getOutputStream().write(buffer, 0, readCount);
-            }
-        }
-        net.getOutputStream().flush();
     }
 }
---
+++ geek-cloud-core/src/main/java/geekbrains/cloud/model/AbstractMessage.java
@@ -0,0 +1,11 @@
+package geekbrains.cloud.model;
+
+import java.io.Serializable;
+
+public abstract class AbstractMessage implements Serializable {
+
+    public abstract MessageType getMessageType();
+
+
+
+}
